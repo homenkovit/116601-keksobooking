40348208338dfbefad0c8709dc0cad018b63cd8b
Vitaly <homenkovit@gmail.com>
2018-02-17 19:43:09 +0300

    Написал функцию драгндропа

--- js/map.js
+++ js/map.js
@@ -4,29 +4,31 @@
   var mapWindow = document.querySelector('.map');
 
   var mainMapPinButton = mapWindow.querySelector('.map__pin--main');
-  var mainMapPinButtonWidth = mainMapPinButton.offsetWidth;
-  var mainMapPinButtonHeight = mainMapPinButton.offsetHeight;
-
-  var getMainMapPinButtonDefaultCoordinate = function (elem) {
-    var box = elem.getBoundingClientRect();
-
-    return {
-      top: box.top + pageYOffset,
-      left: box.left + pageXOffset
+  var mapPinsPanel = mapWindow.querySelector('.map__pins');
+  var mainMapPinButtonWidth = 62;
+  var mainMapPinButtonHeight = 62;
+  var getPosition = function (elem) {
+    var position = {
+      top: elem.offsetTop,
+      left: elem.offsetLeft
     };
+
+    return position;
   };
+  var defaultMainPinButtonPosition = getPosition(mainMapPinButton);
+
   var getPinButtonCoordinate = function (pinButton, defaultState) {
-    var pinButtonDefaultCoordinate = getMainMapPinButtonDefaultCoordinate(pinButton);
+    var pinButtonDefaultCoordinate = getPosition(pinButton);
     var pinButtonCoordinate = {};
     if (defaultState === true) {
       pinButtonCoordinate = {
-        top: pinButtonDefaultCoordinate.top + mainMapPinButtonHeight / 2,
-        left: pinButtonDefaultCoordinate.left + mainMapPinButtonWidth / 2
+        top: pinButtonDefaultCoordinate.top + (mainMapPinButtonHeight / 2),
+        left: pinButtonDefaultCoordinate.left + (mainMapPinButtonWidth / 2)
       };
     } else {
       pinButtonCoordinate = {
-        top: pinButtonDefaultCoordinate.top + mainMapPinButtonHeight + 22,
-        left: pinButtonDefaultCoordinate.left + mainMapPinButtonWidth / 2
+        top: pinButtonDefaultCoordinate.top + (mainMapPinButtonHeight + 22),
+        left: pinButtonDefaultCoordinate.left + (mainMapPinButtonWidth / 2)
       };
     }
 
@@ -43,12 +45,55 @@
     }
     var address = coordinate.left + ', ' + coordinate.top;
     addressInput.value = address;
+
+    return coordinate;
   };
   setAddress(mainMapPinButton, false);
+
+  mainMapPinButton.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((startCoords.y + mainMapPinButtonHeight + 22) < 150 || (startCoords.y + mainMapPinButtonHeight + 22) > 500) {
+        mapPinsPanel.removeEventListener('mousemove', onMouseMove);
+        setAddress(mainMapPinButton, true);
+      }
+
+      mainMapPinButton.style.top = (mainMapPinButton.offsetTop - shift.y) + 'px';
+      mainMapPinButton.style.left = (mainMapPinButton.offsetLeft - shift.x) + 'px';
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      mapPinsPanel.removeEventListener('mousemove', onMouseMove);
+      mapPinsPanel.removeEventListener('mouseup', onMouseUp);
+    };
+
+    mapPinsPanel.addEventListener('mousemove', onMouseMove);
+    mapPinsPanel.addEventListener('mouseup', onMouseUp);
+  });
 
   window.map = {
     mapWindow: mapWindow,
     mainPinButton: mainMapPinButton,
+    defaultMainPinButtonPosition: defaultMainPinButtonPosition,
     setAddress: setAddress
   };
 })();
