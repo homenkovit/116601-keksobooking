a289b9665f7309090bd4b7d35a1c5b5269013873
Vitaly <homenkovit@gmail.com>
2018-02-27 00:38:43 +0300

    Добавил модуль по работе с сетью, переписал функцию генерации пинов и карточек похожих объявлений

---
+++ js/backend.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/keksobooking/data';
+
+  var upload = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+    xhr.addEventListener('error', function () {
+      onError(xhr.error);
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  window.backend = {
+    upload: upload,
+    load: load
+  };
+})();
